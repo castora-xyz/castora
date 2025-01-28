e947485c7a91f866ed367904b07fe7e4664c5577
Jude <jude@castora.xyz>
2025-01-28 11:53:59 +0100

    fix: debug ChainNotConfigured errors

--- frontend/src/contexts/Web3Context.tsx
+++ frontend/src/contexts/Web3Context.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createWeb3Modal } from '@web3modal/wagmi/react';
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { defineChain } from 'viem';
 import { WagmiProvider } from 'wagmi';
 // import { sepolia } from 'wagmi/chains';
@@ -75,6 +75,17 @@ createWeb3Modal({
 });
 
 export const Web3Provider = ({ children }: { children: ReactNode }) => {
+  useEffect(() => {
+    const wagmiStore = localStorage.getItem('wagmi.store');
+    const wagmiData = wagmiStore ? JSON.parse(wagmiStore || '') : '';
+    if (
+      wagmiData &&
+      wagmiConfig.chains.every((c) => c.id !== wagmiData?.chainId)
+    ) {
+      localStorage.removeItem('wagmi.store');
+    }
+  }, []);
+  
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
