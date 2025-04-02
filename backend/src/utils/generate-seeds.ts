b86dcbd7d5dd9cb3f6aec90efe6fc33f0bd16e38
Jude <jude@castora.xyz>
2025-04-02 16:46:10 +0100

    feat: activate magma liquid staking token

--- backend/src/utils/generate-seeds.ts
+++ backend/src/utils/generate-seeds.ts
@@ -1,6 +1,6 @@
 import { PoolSeeds } from '../schemas';
 import { CONTRACT_ADDRESS_SEPOLIA, getContractAddress } from './contract';
-import { /* gMON, */ HYPE, SOL } from './tokens';
+import { gMON, HYPE, SOL } from './tokens';
 import { Chain } from './validate-chain';
 
 const nextNthHour = (current: number, n: number) =>
@@ -54,16 +54,6 @@ export const generateExperimentalsSeeds = (chain: Chain) => {
         })
       );
     }
-
-    // seeds.push(
-    //   new PoolSeeds({
-    //     predictionToken: CONTRACT_ADDRESS_SEPOLIA,
-    //     stakeToken: gMON,
-    //     stakeAmount: 2e17,
-    //     snapshotTime,
-    //     windowCloseTime
-    //   })
-    // );
   }
   return seeds;
 };
@@ -155,6 +145,16 @@ export const generateLiveSeeds = (chain: Chain) => {
         })
       );
     }
+
+    seeds.push(
+      new PoolSeeds({
+        predictionToken: CONTRACT_ADDRESS_SEPOLIA,
+        stakeToken: gMON,
+        stakeAmount: 2e17,
+        snapshotTime,
+        windowCloseTime
+      })
+    );
   }
   return seeds;
 };
