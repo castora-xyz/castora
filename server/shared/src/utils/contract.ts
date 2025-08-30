be81dd45996249aef70cb12312240edd9c49940a
Silas <silas@castora.xyz>
2025-08-30 14:33:24 +0100

    fix: don't throw missing rpc in env if no need for it

--- server/shared/src/utils/contract.ts
+++ server/shared/src/utils/contract.ts
@@ -5,34 +5,35 @@ import { abi } from './abi.js';
 import { Chain } from './index.js';
 import { logger } from './logger.js';
 
-const monadTestnetRpcUrl = process.env.MONAD_TESTNET_RPC_URL;
-if (!monadTestnetRpcUrl) throw 'Set MONAD_TESTNET_RPC_URL';
-
 export const CONTRACT_ADDRESS_SEPOLIA: `0x${string}` = '0x294c2647d9f3eaca43a364859c6e6a1e0e582dbd';
 export const CONTRACT_ADDRESS_MONAD: `0x${string}` = '0xa0742C672e713327b0D6A4BfF34bBb4cbb319C53';
 
-const monadTestnet = defineChain({
-  id: 10143,
-  name: 'Monad Testnet',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'Monad',
-    symbol: 'MON'
-  },
-  rpcUrls: {
-    default: { http: [monadTestnetRpcUrl] }
-  }
-});
+const monadTestnet = () => {
+  if (!process.env.MONAD_TESTNET_RPC_URL) throw 'Set MONAD_TESTNET_RPC_URL in env';
+
+  return defineChain({
+    id: 10143,
+    name: 'Monad Testnet',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'Monad',
+      symbol: 'MON'
+    },
+    rpcUrls: {
+      default: { http: [process.env.MONAD_TESTNET_RPC_URL!] }
+    }
+  });
+};
 
 const getAccount = (chain: Chain) => {
   const adminKeyMonad = process.env.ADMIN_KEY_MONAD as `0x${string}`;
-  if (!adminKeyMonad) throw 'Set ADMIN_KEY_MONAD';
+  if (!adminKeyMonad) throw 'Set ADMIN_KEY_MONAD in env';
   return privateKeyToAccount({ monadtestnet: adminKeyMonad }[chain]);
 };
 
 export const getConfig = (chain: Chain) =>
   ({
-    monadtestnet: { chain: monadTestnet, transport: http() }
+    monadtestnet: { chain: monadTestnet(), transport: http() }
   }[chain]);
 export const getContractAddress = (chain: Chain) =>
   ({
