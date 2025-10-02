c2988d45c8ad1479da806ede94a29a70b5b1b58f
Silas <silas@castora.xyz>
2025-10-02 16:57:15 +0100

    chore: update main Castora fee collector to pools manager contract

--- contract/script/main/UpdateFeeCollector.s.sol
+++ contract/script/main/UpdateFeeCollector.s.sol
@@ -8,8 +8,8 @@ contract UpdateFeeCollector is Script {
   function run() public {
     vm.startBroadcast(vm.envUint('CASTORA_OWNER_KEY'));
     
-    address castoraAddress = vm.envAddress('CASTORA_ADDRESS');
-    address newFeeCollectorAddress = vm.envAddress('NEW_FEE_COLLECTOR_ADDRESS');
+    address castoraAddress = 0xa0742C672e713327b0D6A4BfF34bBb4cbb319C53; // vm.envAddress('CASTORA_ADDRESS');
+    address newFeeCollectorAddress = 0xb4a03C32C7cAa4069f89184f93dfAe065C141061; // vm.envAddress('NEW_FEE_COLLECTOR_ADDRESS');
     
     Castora castora = Castora(payable(castoraAddress));
     castora.setFeeCollector(newFeeCollectorAddress);
