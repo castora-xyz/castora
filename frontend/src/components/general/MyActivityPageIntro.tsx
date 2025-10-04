4b25e4ab24f5d9fc4898472a875bb9038933b7d4
Jude <jude@castora.xyz>
2025-10-04 20:50:02 +0100

    fix: minor UI fix of pinned page navigators for wide screens

--- frontend/src/components/general/MyActivityPageIntro.tsx
+++ frontend/src/components/general/MyActivityPageIntro.tsx
@@ -11,20 +11,22 @@ export const MyActivityPageIntro = ({
   myActivityType: PageIntroType;
 }) => {
   return (
-    <div className="w-full pl-0 top-16 sm:top-[72px] sticky z-10 bg-app-bg p-6 text-sm flex flex-wrap gap-4 text-text-subtitle">
-      <p className="text-sm py-2 px-5 rounded-full w-fit border border-border-default dark:border-surface-subtle text-text-subtitle max-sm:hidden">
-        My Activity
-      </p>
+    <div className="w-full pl-0 top-16 sm:top-[72px] sticky z-10 bg-app-bg p-6 text-sm text-text-subtitle">
+      <div className="flex flex-wrap gap-4 max-w-screen-xl mx-auto">
+        <p className="text-sm py-2 px-5 rounded-full w-fit border border-border-default dark:border-surface-subtle text-text-subtitle max-sm:hidden">
+          My Activity
+        </p>
 
-      <MyActivityPagesMenu placement="my_activity_page_intro">
-        <button className="text-sm py-2 px-5 rounded-full w-fit border border-border-default dark:border-surface-subtle text-text-subtitle p-ripple flex items-center">
-          <span>{title}</span>
-          <ChevronDown className="ml-1 w-4 h-4 sm:w-5 sm:h-5 fill-text-body" />
-          <Ripple />
-        </button>
-      </MyActivityPagesMenu>
+        <MyActivityPagesMenu placement="my_activity_page_intro">
+          <button className="text-sm py-2 px-5 rounded-full w-fit border border-border-default dark:border-surface-subtle text-text-subtitle p-ripple flex items-center">
+            <span>{title}</span>
+            <ChevronDown className="ml-1 w-4 h-4 sm:w-5 sm:h-5 fill-text-body" />
+            <Ripple />
+          </button>
+        </MyActivityPagesMenu>
 
-      {claimAll}
+        {claimAll}
+      </div>
     </div>
   );
 };
--- frontend/src/components/pools/PoolsPageIntro.tsx
+++ frontend/src/components/pools/PoolsPageIntro.tsx
@@ -11,20 +11,22 @@ export const PoolsPageIntro = ({
   poolsPageType: PageIntroType;
 }) => {
   return (
-    <div className="w-full pl-0 top-16 sm:top-[72px] sticky z-10 bg-app-bg p-6 text-sm flex flex-wrap gap-4 text-text-subtitle">
-      <p className="text-sm py-2 px-5 rounded-full w-fit border border-border-default dark:border-surface-subtle text-text-subtitle max-sm:hidden">
-        Pools
-      </p>
+    <div className="w-full pl-0 top-16 sm:top-[72px] sticky z-10 bg-app-bg p-6 text-sm text-text-subtitle">
+      <div className="flex flex-wrap gap-4 max-w-screen-xl mx-auto">
+        <p className="text-sm py-2 px-5 rounded-full w-fit border border-border-default dark:border-surface-subtle text-text-subtitle max-sm:hidden">
+          Pools
+        </p>
 
-      <PoolsPagesMenu placement="pools_page_intro">
-        <button className="text-sm py-2 px-5 rounded-full w-fit border border-border-default dark:border-surface-subtle text-text-subtitle p-ripple flex items-center">
-          <span>{title}</span>
-          <ChevronDown className="ml-1 w-4 h-4 sm:w-5 sm:h-5 fill-text-body" />
-          <Ripple />
-        </button>
-      </PoolsPagesMenu>
+        <PoolsPagesMenu placement="pools_page_intro">
+          <button className="text-sm py-2 px-5 rounded-full w-fit border border-border-default dark:border-surface-subtle text-text-subtitle p-ripple flex items-center">
+            <span>{title}</span>
+            <ChevronDown className="ml-1 w-4 h-4 sm:w-5 sm:h-5 fill-text-body" />
+            <Ripple />
+          </button>
+        </PoolsPagesMenu>
 
-      {filter}
+        {filter}
+      </div>
     </div>
   );
 };
