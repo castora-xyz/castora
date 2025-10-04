196ab66a20b1b6de91d726d6e8fd12e57c1ad397
Silas <silas@castora.xyz>
2025-10-05 00:51:28 +0100

    chore: grant admin role to pools manager contract

--- contract/script/main/GrantAdminRole.s.sol
+++ contract/script/main/GrantAdminRole.s.sol
@@ -8,7 +8,9 @@ contract GrantAdminRole is Script {
   function run() public {
     Castora castora = Castora(payable(0xa0742C672e713327b0D6A4BfF34bBb4cbb319C53));
     vm.startBroadcast(vm.envUint('CASTORA_OWNER_KEY'));
-    castora.grantAdminRole(vm.envAddress('ADMIN_ADDRESS'));
+    address poolsManager = 0xb4a03C32C7cAa4069f89184f93dfAe065C141061;
+    castora.grantAdminRole(poolsManager);
+    // castora.grantAdminRole(vm.envAddress('ADMIN_ADDRESS'));
     vm.stopBroadcast();
   }
 }
