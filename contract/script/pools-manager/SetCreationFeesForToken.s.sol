1f26c2af19a36b6a28bbf7a13d71977f89d9b30d
Silas <silas@castora.xyz>
2025-10-05 13:32:15 +0100

    fix: use pools manager address for paying creation fees in native token

--- contract/script/pools-manager/SetCreationFeesForToken.s.sol
+++ contract/script/pools-manager/SetCreationFeesForToken.s.sol
@@ -7,15 +7,15 @@ import 'forge-std/Script.sol';
 contract SetCreationFeesForToken is Script {
   function run() public {
     vm.startBroadcast(vm.envUint('CASTORA_OWNER_KEY'));
-    
+
     address poolsManagerAddress = 0xb4a03C32C7cAa4069f89184f93dfAe065C141061; // vm.envAddress('POOLS_MANAGER_ADDRESS');
-    address feeTokenAddress = 0xa0742C672e713327b0D6A4BfF34bBb4cbb319C53;// vm.envAddress('CREATION_FEE_TOKEN_ADDRESS');
+    // address feeTokenAddress = vm.envAddress('CREATION_FEE_TOKEN_ADDRESS');
     uint256 feeAmount = 10e18; // 10 MON // vm.envUint('CREATION_FEE_AMOUNT');
 
     CastoraPoolsManager poolsManager = CastoraPoolsManager(payable(poolsManagerAddress));
-    poolsManager.setCreationFees(feeTokenAddress, feeAmount);
-    
-    console.log('Set creation fees for token: ', feeTokenAddress);
+    poolsManager.setCreationFees(poolsManagerAddress, feeAmount);
+
+    console.log('Set creation fees for token: ', poolsManagerAddress);
     console.log('Creation fee amount: ', feeAmount);
     vm.stopBroadcast();
   }
